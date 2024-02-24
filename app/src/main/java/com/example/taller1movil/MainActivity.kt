5b2a0b0e854d7d2e3d2c0fb5203c1a8b618548ef
lopezcg <carlosgablopezm@gmail.com>
2024-02-24 14:40:02 -0500

    Pantalla principal xml

--- app/src/main/java/com/example/taller1movil/MainActivity.kt
+++ app/src/main/java/com/example/taller1movil/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.taller1movil
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
+        val videoView = findViewById<VideoView>(R.id.videoview)
+        val uri = Uri.parse("https://v1.pinimg.com/videos/mc/720p/5f/54/c1/5f54c1e83c67bcb239c6c0c7484d8e12.mp4")
+        videoView.setVideoURI(uri)
+        videoView.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.isLooping = true // Reproducir en bucle
+            val videoWidth = mediaPlayer.videoWidth.toFloat()
+            val videoHeight = mediaPlayer.videoHeight.toFloat()
+            val videoProportion = videoWidth / videoHeight
+
+            val screenWidth = resources.displayMetrics.widthPixels.toFloat()
+            val screenHeight = resources.displayMetrics.heightPixels.toFloat()
+            val screenProportion = screenWidth / screenHeight
+
+            val layoutParams = videoView.layoutParams
+            if (videoProportion > screenProportion) {
+                layoutParams.width = screenWidth.toInt()
+                layoutParams.height = (screenWidth / videoProportion).toInt()
+            } else {
+                layoutParams.width = (videoProportion * screenHeight).toInt()
+                layoutParams.height = screenHeight.toInt()
+            }
+            videoView.layoutParams = layoutParams
+        }
+        videoView.requestFocus()
+        videoView.start()*/
     }
 }
