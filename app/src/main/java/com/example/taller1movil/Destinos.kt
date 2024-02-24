d26e6a4bd352f96cec1695b6f5abf4369597bd72
lopezcg <carlosgablopezm@gmail.com>
2024-02-24 15:37:56 -0500

    Punto 3, lista de destinos segun categoria

--- app/src/main/java/com/example/taller1movil/Destinos.kt
+++ app/src/main/java/com/example/taller1movil/Destinos.kt
@@ -2,10 +2,46 @@ package com.example.taller1movil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
 
 class Destinos : AppCompatActivity() {
+    var arreglo:MutableList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_destinos)
+        val list=findViewById<ListView>(R.id.Li)
+        val adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,arreglo)
+        list.adapter=adapter
+        val json= JSONObject(loadJSONFromAsset())
+        val destinosJson = json.getJSONArray("destinos")
+        val informacion= intent.getBundleExtra("info")
+        for (i in 0 until destinosJson.length()){
+            val jsonObject = destinosJson.getJSONObject(i)
+            if (informacion?.getString("categoria")=="Todos") {
+                arreglo.add(jsonObject.getString("nombre"))
+            }else if(informacion?.getString("categoria")==jsonObject.getString("categoria")){
+                arreglo.add(jsonObject.getString("nombre"))
+            }
+        }
+
+    }
+    fun loadJSONFromAsset(): String? {
+        var json: String? = null
+        try {
+            val isStream: InputStream =assets.open("destinos.json")
+            val size:Int=isStream.available()
+            val buffer = ByteArray(size)
+            isStream.read(buffer)
+            isStream.close()
+            json= String(buffer, Charsets.UTF_8)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
     }
 }
--- app/src/main/java/com/example/taller1movil/MainActivity.kt
+++ app/src/main/java/com/example/taller1movil/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.taller1movil
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
 import android.widget.MediaController
+import android.widget.Spinner
 import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val BTN1 = findViewById<Button>(R.id.button1)
+        val BTN2 = findViewById<Button>(R.id.button2)
+        val BTN3 = findViewById<Button>(R.id.button3)
+        val spinner=findViewById<Spinner>(R.id.spinner1)
+        val intent= Intent(this,Destinos::class.java)
+        val bundle=Bundle()
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val categoriaSeleccionada = parent?.getItemAtPosition(position).toString()
+                bundle.putString("categoria", categoriaSeleccionada)
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Another interface callback.
+            }
+        }
+        intent.putExtra("info",bundle)
+        BTN1.setOnClickListener{
+            startActivity(intent)
+        }
         /*
         val videoView = findViewById<VideoView>(R.id.videoview)
         val uri = Uri.parse("https://v1.pinimg.com/videos/mc/720p/5f/54/c1/5f54c1e83c67bcb239c6c0c7484d8e12.mp4")
